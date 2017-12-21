5816ce811f8f7d8f427868488cea09c13ad9ea2d
Jdville03 <jarrel.delottinville@gmail.com>
2017-12-21 11:27:01 -0500

    Connect React app to Rails API with /api/players call

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import './App.css';
+import Players from './Players';
+
+const API_URL = process.env.REACT_APP_API_URL;
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      players: []
+    }
+  }
+
+  componentDidMount() {
+    fetch(`${API_URL}/players`)
+      .then(response => response.json())
+      .then(players => this.setState({ players }))
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">App Container</h1>
+          {/* <h1 className="App-title">App Container</h1> */}
         </header>
+        <Players players={this.state.players} />
+        {/*}
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        */}
       </div>
     );
   }
---
+++ src/containers/Players.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Players.css';
+
+const Players = ({ players }) => (
+  <div className="PlayersContainer">
+    <h1>Players Component</h1>
+    {players.map(player =>
+      <div className="PlayerCard" key={player.id}>
+        <img src={player.image_url} alt={player.last_name} />
+        <h3>{player.first_name} {player.last_name}</h3>
+        <p>#{player.jersey_number} - {player.position}</p>
+      </div>  
+    )}
+  </div>
+);
+
+export default Players;
