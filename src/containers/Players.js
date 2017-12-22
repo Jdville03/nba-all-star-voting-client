44c67b20bb2e6912734ff3d6225349ad601f6af5
Jdville03 <jarrel.delottinville@gmail.com>
2017-12-21 23:53:27 -0500

    Update Player styling

--- src/containers/Players.js
+++ src/containers/Players.js
@@ -6,7 +6,7 @@ const Players = ({ players }) => (
     <h1>Players Component</h1>
     {players.map(player =>
       <div className="PlayerCard" key={player.id}>
-        <img src={player.image_url} alt={player.last_name} />
+        <img src={player.image_url} alt={player.last_name} className="PlayerImage" />
         <h3>{player.first_name} {player.last_name}</h3>
         <p>#{player.jersey_number} - {player.position}</p>
       </div>  
