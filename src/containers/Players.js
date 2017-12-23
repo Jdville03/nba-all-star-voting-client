160fcbbc81b4136c7a1b5fc77b1828980d89bc03
Jdville03 <jarrel.delottinville@gmail.com>
2017-12-22 21:34:18 -0500

    Move presentation from Players component to PlayerCard component

--- src/containers/Players.js
+++ src/containers/Players.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import './Players.css';
+import PlayerCard from '../components/PlayerCard';
 
 const Players = ({ players }) => (
   <div className="PlayersContainer">
     <h1>Players Component</h1>
-    {players.map(player =>
-      <div className="PlayerCard" key={player.id}>
-        <img src={player.image_url} alt={player.last_name} className="PlayerImage" />
-        <h3>{player.first_name} {player.last_name}</h3>
-        <p>#{player.jersey_number} - {player.position}</p>
-      </div>  
-    )}
+    {players.map(player => <PlayerCard player={player} key={player.id} />)}
   </div>
 );
 
