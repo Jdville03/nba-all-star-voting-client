33740f0355cf83fb6a7161e5cec10d0c2d3a673a
Jdville03 <jarrel.delottinville@gmail.com>
2017-12-23 17:23:36 -0500

    Remove state and fetch from App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,22 +3,7 @@ import logo from '../logo.svg';
 import './App.css';
 import Players from './Players';
 
-const API_URL = process.env.REACT_APP_API_URL;
-
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      players: []
-    }
-  }
-
-  componentDidMount() {
-    fetch(`${API_URL}/players`)
-      .then(response => response.json())
-      .then(players => this.setState({ players }))
-  }
 
   render() {
     return (
@@ -27,7 +12,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           {/* <h1 className="App-title">App Container</h1> */}
         </header>
-        <Players players={this.state.players} />
+        <Players />
         {/*}
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
