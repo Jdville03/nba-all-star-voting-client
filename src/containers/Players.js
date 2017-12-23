96ea79f50231b4f167e0ea5eeaa567010e28b43d
Jdville03 <jarrel.delottinville@gmail.com>
2017-12-23 17:27:47 -0500

    Connect Redux store and actions to props of Players component

--- src/containers/Players.js
+++ src/containers/Players.js
@@ -1,12 +1,44 @@
-import React from 'react';
-import './Players.css';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { bindActionCreators } from 'redux';
+import { fetchPlayers } from '../actions/playerActions';
 import PlayerCard from '../components/PlayerCard';
+import PlayerForm from './PlayerForm';
+import './Players.css';
 
-const Players = ({ players }) => (
-  <div className="PlayersContainer">
-    <h1>Players Component</h1>
-    {players.map(player => <PlayerCard player={player} key={player.id} />)}
-  </div>
-);
+class Players extends Component {
+  
+  componentDidMount() {
+    this.props.fetchPlayers();
+  }
 
-export default Players;
+  render() {
+    return (
+      <div className="PlayersContainer">
+        <h1>Players Component</h1>
+        {this.props.players.map(player => (
+          <PlayerCard player={player} key={player.id} />
+        ))}
+        <PlayerForm />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    players: state.players
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators(
+    {
+      fetchPlayers: fetchPlayers
+    },
+    dispatch
+  );
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Players);
