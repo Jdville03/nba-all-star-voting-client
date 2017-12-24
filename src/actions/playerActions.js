c2bcfc628f709930b0f3f802173bac80914d093c
Jdville03 <jarrel.delottinville@gmail.com>
2017-12-24 02:22:20 -0500

    Add addPlayer action to playerActions.js

--- src/actions/playerActions.js
+++ src/actions/playerActions.js
@@ -1,4 +1,5 @@
 import fetch from "isomorphic-fetch";
+import { resetPlayerForm } from './playerForm';
 
 const API_URL = process.env.REACT_APP_API_URL;
 
@@ -7,8 +8,26 @@ export function fetchPlayers() {
     return fetch(`${API_URL}/players`)
       .then(response => response.json())
       .then(players => dispatch({ type: "FETCH_PLAYERS", players }))
-      .catch (error => console.log(error));
+      .catch(error => console.log(error));
   };
-}
+};
+
+export function addPlayer(player) {
+  return dispatch => {
+    return fetch(`${API_URL}/players`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ player: player })
+    })
+      .then(response => response.json())
+      .then(player => {
+        dispatch({ type: "ADD_PLAYER", player })
+        dispatch(resetPlayerForm())
+      })
+      .catch(error => console.log(error));
+  };
+};
 
 
