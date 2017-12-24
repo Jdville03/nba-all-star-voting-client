ff411008b1c831ad074c6d0b29b86205e74079d0
Jdville03 <jarrel.delottinville@gmail.com>
2017-12-24 02:20:37 -0500

    Move App component and related files from containers folder to components folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import './App.css';
-import Players from './Players';
+import Players from '../containers/Players';
 
 class App extends Component {
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
 import { store } from "./store.js";
-import App from './containers/App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
