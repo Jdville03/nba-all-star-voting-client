3a5065b0115359a6441b5c14318771127d51ed54
Jdville03 <jarrel.delottinville@gmail.com>
2017-12-26 15:54:23 -0500

    Add teams reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import players from './players';
 import playerFormData from './playerFormData';
+import teams from './teams';
 
 export default combineReducers({
   players,
-  playerFormData
+  playerFormData,
+  teams
 });
