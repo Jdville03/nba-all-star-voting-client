5fc25702fc98585115b99e06f9f3373425786d47
Jdville03 <jarrel.delottinville@gmail.com>
2017-12-26 02:56:04 -0500

    Change App title

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          {/* <h1 className="App-title">App Container</h1> */}
+          <h1 className="App-title">NBA All Star Voting</h1>
         </header>
         <Players />
         {/*}
