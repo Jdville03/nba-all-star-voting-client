2f3693d0cb779d78acce4068f213ef9411b9bab9
Jdville03 <jarrel.delottinville@gmail.com>
2017-12-27 01:59:14 -0500

    Incorporate semantic styling and move player form data from redux store to PlayerForm component level state

--- src/containers/PlayerForm.js
+++ src/containers/PlayerForm.js
@@ -1,72 +1,103 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { updatePlayerFormData } from '../actions/playerForm';
 import { addPlayer } from '../actions/playerActions';
 import { fetchTeams } from '../actions/teamActions';
+import { Form } from 'semantic-ui-react';
 
 class PlayerForm extends Component {
-  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      last_name: "",
+      first_name: "",
+      team_id: "",
+      position: "",
+      image_url: ""
+    };
+  }
+
   componentDidMount() {
     this.props.fetchTeams();
   }
 
   handleOnChange = event => {
     const { name, value } = event.target;
-    const currentPlayerFormData = Object.assign({}, this.props.playerFormData, {
+    this.setState({
       [name]: value
     });
-    this.props.updatePlayerFormData(currentPlayerFormData);
   };
+
+  handleOnSelectChange = (e, { value }) => this.setState({
+    team_id: value
+  });
 
   handleOnSubmit = event => {
     event.preventDefault();
-    this.props.addPlayer(this.props.playerFormData);
+    this.props.addPlayer(this.state);
+    this.setState({
+      last_name: "",
+      first_name: "",
+      team_id: "",
+      position: "",
+      image_url: ""
+    });
   };
 
   render() {
-    const {
-      last_name,
-      first_name,
-      team_id,
-      position,
-      image_url
-    } = this.props.playerFormData;
+    const { last_name, first_name, team_id, position, image_url } = this.state;
 
-    const renderTeamsOptions = this.props.teams.map(team =>
-      <option value={team.id}>{team.city} {team.name}</option>
-    );
+    const renderTeamsOptions = this.props.teams.map(team => {
+      return Object.assign({}, { key: team.id, text: `${team.city} ${team.name}`, value: team.id });
+    });
 
     return (
       <div>
         <h3>Add a Player to the Ballot</h3>
-        <form onSubmit={this.handleOnSubmit}>
-          <div>
-            {/* <label htmlFor="last_name">Last Name: </label> */}
-            <input type="text" placeholder="Last Name" name="last_name" value={last_name} onChange={this.handleOnChange} />
-          </div>
-          <div>
-            {/* <label htmlFor="first_name">First Name: </label> */}
-            <input type="text" placeholder="First Name" name="first_name" value={first_name} onChange={this.handleOnChange} />
-          </div>
-          <div>
-            {/* <label htmlFor="team_id">Team: </label> */}
-            <select name="team_id" value={team_id} onChange={this.handleOnChange}>
-              <option value="" disabled>
-                Select Team
-              </option>
-              {renderTeamsOptions}
-            </select>
-          </div>
-          <div>
-            {/* <label htmlFor="position">Position: </label> */}
-            <input type="text" placeholder="Position" name="position" value={position} onChange={this.handleOnChange} />
-          </div>
-          <div>
-            {/* <label htmlFor="image_url">Image URL: </label> */}
-            <input type="text" placeholder="Image URL" name="image_url" value={image_url} onChange={this.handleOnChange} />
-          </div>
-          <button type="submit">Add Player</button>
-        </form>
+        <Form onSubmit={this.handleOnSubmit}>
+          <Form.Group widths="equal">
+            <Form.Input
+              label="Last name"
+              placeholder="Last name"
+              name="last_name"
+              value={last_name}
+              onChange={this.handleOnChange}
+            />
+            <Form.Input
+              label="First name"
+              placeholder="First name"
+              name="first_name"
+              value={first_name}
+              onChange={this.handleOnChange}
+            />
+          </Form.Group>
+          <Form.Group widths="equal">
+            <Form.Select
+              label="Team"
+              options={renderTeamsOptions}
+              placeholder="Select Team"
+              name="team_id"
+              selection
+              value={team_id}
+              onChange={this.handleOnSelectChange}
+            />
+            <Form.Input
+              label="Position"
+              placeholder="Position"
+              name="position"
+              value={position}
+              onChange={this.handleOnChange}
+            />
+            <Form.Input
+              label="Image URL"
+              placeholder="Image URL"
+              name="image_url"
+              value={image_url}
+              onChange={this.handleOnChange}
+            />
+          </Form.Group>
+          <Form.Button>Add Player</Form.Button>
+        </Form>
       </div>
     );
   }
@@ -74,9 +105,8 @@ class PlayerForm extends Component {
 
 const mapStateToProps = state => {
   return {
-    playerFormData: state.playerFormData,
     teams: state.teams
   }
 }
 
-export default connect(mapStateToProps, { updatePlayerFormData, addPlayer, fetchTeams })(PlayerForm);
+export default connect(mapStateToProps, { addPlayer, fetchTeams })(PlayerForm);
