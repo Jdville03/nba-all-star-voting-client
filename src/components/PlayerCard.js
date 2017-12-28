01c041c52544aca5e945980fea46216e92f29236
Jdville03 <jarrel.delottinville@gmail.com>
2017-12-28 00:16:33 -0500

    Update PlayerCard.js presentation

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -1,21 +1,41 @@
 import React from 'react';
 
+import { Icon, Item } from "semantic-ui-react";
+
 const PlayerCard = ({ player }) => (
-  <div className="PlayerCard">
-    <img
-      src={player.image_url}
-      alt={player.last_name}
-      className="PlayerImage"
-    />
-    <h3>
-      {player.last_name}, {player.first_name}
-      <br />
-      <small>
-        {player.team.conference} / {player.team.abbreviation} / {player.position}
-      </small>
-    </h3>
-    {/* <p>#{player.jersey_number} - {player.position}</p> */}
-  </div>
+  // <Grid.Column>
+    <Item>
+      <Item.Image size="small" src={player.image_url} />
+
+      <Item.Content verticalAlign="middle">
+        <Item.Header as="a">
+          {player.last_name}, {player.first_name}
+        </Item.Header>
+        <Item.Description>
+          {player.team.conference} / {player.team.abbreviation} /{" "}
+          {player.position}
+        </Item.Description>
+        <Item.Extra>
+          <Icon color="green" name="check" /> {player.id} Votes
+        </Item.Extra>
+      </Item.Content>
+    </Item>
+  // </Grid.Column>
+
+  // <div className="PlayerCard">
+  //   <img
+  //     src={player.image_url}
+  //     alt={player.last_name}
+  //     className="PlayerImage"
+  //   />
+  //   <h3>
+  //     {player.last_name}, {player.first_name}
+  //     <br />
+  //     <small>
+  //       {player.team.conference} / {player.team.abbreviation} / {player.position}
+  //     </small>
+  //   </h3>
+  // </div>
 );
 
 export default PlayerCard;
