5b6af49cefb4719993ab20b1e593811190e6e6fa
Jdville03 <jarrel.delottinville@gmail.com>
2017-12-29 01:16:15 -0500

    Change presentation from Item to Card in PlayerCard component

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -1,41 +1,30 @@
 import React from 'react';
+import VotingButton from './VotingButton';
 
-import { Icon, Item } from "semantic-ui-react";
+import { Card, Image } from 'semantic-ui-react';
 
-const PlayerCard = ({ player }) => (
-  // <Grid.Column>
-    <Item>
-      <Item.Image size="small" src={player.image_url} />
+const PlayerCard = ({ player }) => {
+  const cardColor = player.team.conference === "Western" ? "red" : "blue";
 
-      <Item.Content verticalAlign="middle">
-        <Item.Header as="a">
+  return (
+    <Card color={cardColor}>
+      <Image size="small" centered src={player.image_url} />
+      <Card.Content>
+        <Card.Header>
           {player.last_name}, {player.first_name}
-        </Item.Header>
-        <Item.Description>
-          {player.team.conference} / {player.team.abbreviation} /{" "}
+        </Card.Header>
+        <Card.Meta>
           {player.position}
-        </Item.Description>
-        <Item.Extra>
-          <Icon color="green" name="check" /> {player.id} Votes
-        </Item.Extra>
-      </Item.Content>
-    </Item>
-  // </Grid.Column>
-
-  // <div className="PlayerCard">
-  //   <img
-  //     src={player.image_url}
-  //     alt={player.last_name}
-  //     className="PlayerImage"
-  //   />
-  //   <h3>
-  //     {player.last_name}, {player.first_name}
-  //     <br />
-  //     <small>
-  //       {player.team.conference} / {player.team.abbreviation} / {player.position}
-  //     </small>
-  //   </h3>
-  // </div>
-);
+        </Card.Meta>
+        <Card.Description>
+          {player.team.conference} / {player.team.abbreviation}
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <VotingButton player={player} />
+      </Card.Content>
+    </Card>
+  );
+};
 
 export default PlayerCard;
