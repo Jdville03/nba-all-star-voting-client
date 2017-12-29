7699702d04904dfd10cd79665590c811ed26a872
Jdville03 <jarrel.delottinville@gmail.com>
2017-12-29 01:15:03 -0500

    Remove use of classes for custom css styling in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -7,17 +7,21 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <h1 className="App-title">NBA All Star Voting</h1>
+      //   </header>
+      //   <p className="App-intro">
+      //     To get started, edit <code>src/App.js</code> and save to reload.
+      //   </p>
+      // </div>
+
+      <div>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">NBA All Star Voting</h1>
+          <h1>NBA All Star Voting</h1>
         </header>
         <Players />
-        {/*}
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        */}
       </div>
     );
   }
