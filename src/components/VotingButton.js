4c4914d5ad8c91cd0e6c243cda82e800f701632f
Jdville03 <jarrel.delottinville@gmail.com>
2017-12-29 17:00:04 -0500

    Update VotingButton component to handle vote click

--- src/components/VotingButton.js
+++ src/components/VotingButton.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Button, Icon, Label } from 'semantic-ui-react';
 
-const VotingButton = ({ player }) => (
-  <Button as="div" labelPosition="right">
-    <Button basic color="green">
-      <Icon name="check" />
-      Vote
+
+const VotingButton = ({ player, upVotePlayer }) => {
+  const ButtonColor = player.team.conference === "Western" ? "red" : "blue";
+
+  return (
+    <Button as="div" labelPosition="right" onClick={() => upVotePlayer(player.id, player.votes + 1)}>
+      <Button basic color={ButtonColor}>
+        <Icon name="check" />
+        Vote
+      </Button>
+      <Label as="a" color={ButtonColor} pointing="left">
+        {player.votes}
+      </Label>
     </Button>
-    <Label as="a" color="green" pointing="left">
-      {player.id}
-      {/* {player.votes} */}
-    </Label>
-  </Button>
-);
+  );  
+};
 
 export default VotingButton;
