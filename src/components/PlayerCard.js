093875ed63f2c073e94369ef0b1e1f8861786206
Jdville03 <jarrel.delottinville@gmail.com>
2017-12-30 01:28:56 -0500

    Provide default image replacement for broken image input in PlayerCard component

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -6,9 +6,13 @@ import { Card, Image } from 'semantic-ui-react';
 const PlayerCard = ({ player, upVotePlayer }) => {
   const cardColor = player.team.conference === "Western" ? "red" : "blue";
 
+  const addDefaultSrc = (event) => {
+    event.target.src = "https://vote.nba.com/static/media/Logo_NBA_grey.36d9a907.svg"
+  }
+
   return (
     <Card color={cardColor}>
-      <Image size="small" centered src={player.image_url} />
+      <Image size="small" centered src={player.image_url} onError={addDefaultSrc} />
       <Card.Content>
         <Card.Header>
           {player.last_name}, {player.first_name}
