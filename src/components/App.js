5d61b21da927d0f9bcc13e994cfafbd6b1bd4ba0
Jdville03 <jarrel.delottinville@gmail.com>
2018-01-02 03:34:37 -0500

    Refactor to incorporate top level state container

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,19 @@
 import React, { Component } from 'react';
-// import logo from '../logo.svg';
-import './App.css';
-import Players from '../containers/Players';
+// import './App.css';
+import PlayersDashboard from '../containers/PlayersDashboard';
+
+import { Header, Image } from 'semantic-ui-react';
 
 class App extends Component {
 
   render() {
     return (
-      // <div className="App">
-      //   <header className="App-header">
-      //     <img src={logo} className="App-logo" alt="logo" />
-      //     <h1 className="App-title">NBA All Star Voting</h1>
-      //   </header>
-      //   <p className="App-intro">
-      //     To get started, edit <code>src/App.js</code> and save to reload.
-      //   </p>
-      // </div>
-
       <div>
-        <header className="App-header">
-          <h1>NBA All Star Voting</h1>
-        </header>
-        <Players />
+        <Header as="h1" textAlign="center" block>
+          <Image src="https://espntheundefeated.files.wordpress.com/2017/05/nba-logo.png" size="medium" />
+          NBA All Star Ballot
+        </Header>
+        <PlayersDashboard />
       </div>
     );
   }
