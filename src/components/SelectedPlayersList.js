cea642c6ed181472e9ddcfcc763dbb55f41fc573
Jdville03 <jarrel.delottinville@gmail.com>
2018-01-02 05:20:20 -0500

    Update SelectedPlayersList component to handle placeholder players

--- src/components/SelectedPlayersList.js
+++ src/components/SelectedPlayersList.js
@@ -4,38 +4,88 @@ import PlayerListItem from './PlayerListItem';
 import { List, Grid, Header, Image, Segment } from 'semantic-ui-react';
 
 const SelectedPlayersList = ({ players }) => {
-
-  const eastPlayers = players.filter(player => player.team.conference === "Eastern");
-
-  const topEastFrontcourtPlayers = eastPlayers
-    .filter(player => player.position === "Frontcourt")
-    .sort((a, b) => b.votes - a.votes)
-    .slice(0, 3);
   
-  const topEastGuards = eastPlayers
-    .filter(player => player.position === "Guard")
-    .sort((a, b) => b.votes - a.votes)
-    .slice(0, 2);
-  
-  const topEastPlayers = topEastFrontcourtPlayers.concat(topEastGuards);
-  
-  const westPlayers = players.filter(player => player.team.conference === "Western");
+  const topEastPlayers = () => {
+    const eastPlaceholderPlayer = {
+      team: { conference: "Eastern" },
+      position: "Frontcourt",
+      image_url: "https://vote.nba.com/static/media/Logo_NBA_grey.36d9a907.svg",
+      votes: 0
+    };
 
-  const topWestFrontcourtPlayers = westPlayers
-    .filter(player => player.position === "Frontcourt")
-    .sort((a, b) => b.votes - a.votes)
-    .slice(0, 3);
+    const eastPlayers = players.filter(player => player.team.conference === "Eastern");
 
-  const topWestGuards = westPlayers
-    .filter(player => player.position === "Guard")
-    .sort((a, b) => b.votes - a.votes)
-    .slice(0, 2);
-  
-  const topWestPlayers = topWestFrontcourtPlayers.concat(topWestGuards);
+    const topEastFrontcourtPlayers = () => {
+      let frontcourtPlayers = eastPlayers
+        .filter(player => player.position === "Frontcourt" && player.votes > 0)
+        .sort((a, b) => b.votes - a.votes)
+        .slice(0, 3);
+
+      while (frontcourtPlayers.length < 3) {
+        frontcourtPlayers.push(eastPlaceholderPlayer);
+      }
+      return frontcourtPlayers;
+    };
+
+    const topEastGuards = () => {
+      let guards = eastPlayers
+        .filter(player => player.position === "Guard" && player.votes > 0)
+        .sort((a, b) => b.votes - a.votes)
+        .slice(0, 2);
+
+      while (guards.length < 2) {
+        guards.push(Object.assign({}, eastPlaceholderPlayer, {
+          position: "Guard"
+        }));
+      }
+      return guards;
+    };
+
+    return topEastFrontcourtPlayers().concat(topEastGuards());
+  };
+
+  const topWestPlayers = () => {
+    const westPlaceholderPlayer = {
+      team: { conference: "Western" },
+      position: "Frontcourt",
+      image_url: "https://vote.nba.com/static/media/Logo_NBA_grey.36d9a907.svg",
+      votes: 0
+    };
+
+    const westPlayers = players.filter(player => player.team.conference === "Western");
+
+    const topWestFrontcourtPlayers = () => {
+      let frontcourtPlayers = westPlayers
+        .filter(player => player.position === "Frontcourt" && player.votes > 0)
+        .sort((a, b) => b.votes - a.votes)
+        .slice(0, 3);
+
+      while (frontcourtPlayers.length < 3) {
+        frontcourtPlayers.push(westPlaceholderPlayer);
+      }
+      return frontcourtPlayers;
+    };
+
+    const topWestGuards = () => {
+      let guards = westPlayers
+        .filter(player => player.position === "Guard" && player.votes > 0)
+        .sort((a, b) => b.votes - a.votes)
+        .slice(0, 2);
+
+      while (guards.length < 2) {
+        guards.push(Object.assign({}, westPlaceholderPlayer, {
+          position: "Guard"
+        }));
+      }
+      return guards;
+    };
+
+    return topWestFrontcourtPlayers().concat(topWestGuards());
+  };
   
   return (
     <Segment inverted color="black">
-      <Header inverted as="h3" dividing textAlign="center">
+      <Header inverted as="h3" textAlign="center">
         <Header.Content>
           Current Selected Starters
           <Header.Subheader>
@@ -51,10 +101,10 @@ const SelectedPlayersList = ({ players }) => {
               Eastern Conference
             </Header>  
             <List>
-              {topEastPlayers.map(player =>
+              {topEastPlayers().map((player, index) =>
                 <PlayerListItem
                   player={player}
-                  key={player.id}
+                  key={index}
                 />  
               )}
             </List>
@@ -67,10 +117,10 @@ const SelectedPlayersList = ({ players }) => {
               Western Conference
             </Header>  
             <List>
-              {topWestPlayers.map(player =>
+              {topWestPlayers().map((player, index) =>
                 <PlayerListItem
                   player={player}
-                  key={player.id}
+                  key={index}
                 />  
               )}
             </List>
