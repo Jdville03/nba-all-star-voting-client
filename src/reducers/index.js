04a28c6d3fdddba4da4d53d687d3a0c17fe3fb8e
Jdville03 <jarrel.delottinville@gmail.com>
2018-01-03 11:03:40 -0500

    Add actions and reducer for selectedPlayers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import players from './players';
 import teams from './teams';
+import selectedPlayers from './selectedPlayers';
 
 export default combineReducers({
   players,
-  teams
+  teams,
+  selectedPlayers
 });
---
+++ src/reducers/selectedPlayers.js
@@ -0,0 +1,8 @@
+export default (state = { topEastFrontcourtPlayers: [], topEastGuards: [], topWestFrontcourtPlayers: [], topWestGuards: [] }, action) => {
+
+  switch (action.type) {
+    
+    default:
+      return state;
+  }
+};
