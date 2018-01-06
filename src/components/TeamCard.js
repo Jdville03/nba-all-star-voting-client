99d210ca5b71b272e4d276af7992d50c3441f938
Jdville03 <jarrel.delottinville@gmail.com>
2018-01-05 19:13:01 -0500

    Add react-router Link to TeamCard component

--- src/components/TeamCard.js
+++ src/components/TeamCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 import { Card, Image } from 'semantic-ui-react';
 import Pluralize from 'pluralize';
 
@@ -9,14 +9,14 @@ const TeamCard = ({ team }) => {
   const playersOnBallot = Pluralize("player", team.players.length, true);
 
   return (
-    <Card color={cardColor} href={`/teams/${team.id}`}>
+    <Card color={cardColor} as={Link} to={`/teams/${team.id}`}>
       <Image src={team.image_url} size="tiny" centered />
       <Card.Content>
         <Card.Header>
           {team.city} {team.name}
         </Card.Header>
         <Card.Meta>
-          {team.conference}
+          {team.conference.substring(0, 4)}
         </Card.Meta>
       </Card.Content>
       <Card.Content extra>
