6c647d6d9b6b38d6dbf7f94155bbeab5c9a28b6d
Jdville03 <jarrel.delottinville@gmail.com>
2018-01-05 19:13:27 -0500

    Incorporate routes into Teams component

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,23 +1,39 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import TeamCard from './TeamCard';
+import Players from './Players';
 
 import { Card } from 'semantic-ui-react';
 
-const Teams = ({ players, teams, upVotePlayer, removePlayer, selectedPlayers }) => {
+const Teams = ({ players, teams, upVotePlayer, removePlayer, selectedPlayers, match }) => {
 
   const sortedTeams = teams.sort((a, b) => (
     a.city.localeCompare(b.city) || a.name.localeCompare(b.name)
   ));
 
   return (
-    <Card.Group itemsPerRow={6} doubling>
-      {sortedTeams.map(team =>
-        <TeamCard
-          team={team}
-          key={team.id}
+    <div>
+      <Route exact path={match.url} render={() => (
+        <Card.Group itemsPerRow={6} doubling>
+          {sortedTeams.map(team =>
+            <TeamCard
+              team={team}
+              key={team.id}
+            />
+          )}
+        </Card.Group>
+      )} />
+      <Route path={`${match.url}/:teamId`} render={(props) => (
+        <Players
+          players={players}
+          teams={teams}
+          upVotePlayer={upVotePlayer}
+          removePlayer={removePlayer}
+          selectedPlayers={selectedPlayers}
+          {...props}
         />
-      )}
-    </Card.Group>
+      )} />
+    </div>
   );
 };
 
